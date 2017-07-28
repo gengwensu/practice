7081b4a0d017a5f410233c734a87d91428d084d3
Gary Su <gengwensu@yahoo.com>
2017-07-28 16:19:37 -0400

    test1

--- mycomma.go
+++ mycomma.go
@@ -8,6 +8,7 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"strings"
 )
 
 var Istr = flag.String("Istr", "abc", "input str for IsUnique")
@@ -15,10 +16,22 @@ var Istr = flag.String("Istr", "abc", "input str for IsUnique")
 func main() {
 	flag.Parse()
 	fmt.Printf("Input string %s\n", *Istr)
-	fmt.Println(mycomma(*Istr))
+	fmt.Println(myComma(*Istr))
 }
 
-func mycomma(s string) string {
+func myComma(s string) string {
+	switch strings.Count(s, ".") {
+	case 1:
+		sl := strings.Split(s, ".") //split the string into two substring
+		return intComma(sl[0]) + "." + intComma(sl[1])
+	case 0:
+		return intComma(s)
+	default:
+		return "illegal format"
+	}
+}
+
+func intComma(s string) string {
 	n := len(s) //length, assuming all numbers
 	if n <= 3 {
 		return s
