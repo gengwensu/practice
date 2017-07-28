c96996254732f5b4bf689ab54df98b5f8803d881
Gary Su <gengwensu@yahoo.com>
2017-07-28 15:35:05 -0400

    test

---
+++ mycomma.go
@@ -0,0 +1,40 @@
+/*
+Comma: Non-recursive version of comma, using bytes,Buffer instead of string
+concatenation
+*/
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+)
+
+var Istr = flag.String("Istr", "abc", "input str for IsUnique")
+
+func main() {
+	flag.Parse()
+	fmt.Printf("Input string %s\n", *Istr)
+	fmt.Println(mycomma(*Istr))
+}
+
+func mycomma(s string) string {
+	n := len(s) //length, assuming all numbers
+	if n <= 3 {
+		return s
+	}
+	var buf bytes.Buffer
+	rem := n % 3
+	if rem > 0 {
+		buf.WriteString(s[:rem])
+	}
+	for i := rem; i < n; i += 3 {
+		if i > 0 {
+			buf.WriteByte(',')
+		}
+
+		buf.WriteString(s[i : i+3])
+
+	}
+	return buf.String()
+}
